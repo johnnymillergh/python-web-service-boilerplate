2ae5f063c05ec6425658b73e2ed8141583014d4c
Johnny Miller <johnnysviva@outlook.com>
2025-08-31 07:46:47 +0800

    test: update user registration test to use UserRegistration schema

--- tests/test_python_web_service_boilerplate/system/auth/test_router.py
+++ tests/test_python_web_service_boilerplate/system/auth/test_router.py
@@ -2,22 +2,21 @@ from faker import Faker
 from loguru import logger
 from starlette.testclient import TestClient
 
+from python_web_service_boilerplate.system.auth.schemas import UserRegistration
 
-def test_root(test_client: TestClient) -> None:
+
+def test_user_registration(test_client: TestClient) -> None:
     faker = Faker()
     first_name = faker.first_name()
     last_name = faker.last_name()
-    response = test_client.post(
-        url="/api/v1/users",
-        json={
-            "username": f"{first_name.lower()}.{last_name.lower()}",
-            "password": "123456",
-            "email": f"{faker.email()}",
-            "full_name": f"{first_name} {last_name}",
-        },
+    user_registration = UserRegistration(
+        username=f"{first_name.lower()}.{last_name.lower()}",
+        password=f"{faker.password()}",
+        email=faker.email(),
+        full_name=f"{first_name} {last_name}",
     )
+    response = test_client.post(url="/api/v1/users", json=user_registration.model_dump())
     response_json = response.json()
-    logger.info(f"Response: {response_json}")
+    logger.info(f"User registration response: {response}")
     assert response.status_code == 200
-    expected_keys = {"username", "password", "email", "full_name"}
-    assert set(response_json.keys()) == expected_keys
+    assert set(response_json.keys()) == user_registration.model_dump().keys()
