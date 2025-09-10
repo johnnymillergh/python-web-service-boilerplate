fb86a8aca55a3a951fc3f1ecdeed3d1eb194601b
Johnny Miller <johnnysviva@outlook.com>
2025-09-10 14:04:09 +0800

    build: remove unused-import

--- src/python_web_service_boilerplate/core/auth/models.py
+++ src/python_web_service_boilerplate/core/auth/models.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from datetime import datetime, timezone
+from datetime import datetime
 
 from advanced_alchemy.extensions.fastapi import (
     base,
@@ -17,18 +17,14 @@ from python_web_service_boilerplate.core.common_models import Deleted
 class User(base.BigIntBase):
     __tablename__ = "user"
 
-    username: Mapped[str] = mapped_column(
-        String(length=64), index=True, unique=True, comment="The username"
-    )
+    username: Mapped[str] = mapped_column(String(length=64), index=True, unique=True, comment="The username")
     password: Mapped[str] = mapped_column(String(length=512), comment="The password")
     email: Mapped[str] = mapped_column(String(length=256), index=True, comment="The email address")
     full_name: Mapped[str] = mapped_column(String(length=128), comment="The full name of the user")
     scopes: Mapped[str] = mapped_column(Text(), comment="The scopes/permissions assigned to the user, comma-separated")
 
     # Common audit fields
-    created_by: Mapped[str] = mapped_column(
-        String(length=64), default=get_login_user, comment="Created by"
-    )
+    created_by: Mapped[str] = mapped_column(String(length=64), default=get_login_user, comment="Created by")
     created_at: Mapped[datetime] = mapped_column(
         default=lambda: datetime.now().astimezone(), comment="Creation timestamp"
     )
